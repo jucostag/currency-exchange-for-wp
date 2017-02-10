906fb2fc9ca2b46cb95201b418ee5069c4717067
Juliana Goncalves <juliana.goncosta@gmail.com>
2017-02-10 14:06:47 -0200

    Updating plugin version and enqueues

--- ce.php
+++ ce.php
@@ -4,7 +4,7 @@
 * Plugin Name: Currency Exchange for WP
 * Plugin URI: https://github.com/jucostag/currency-exchange-for-wp
 * Description: Provides a currency conversion calculator for wordpress based on information provided by Yahoo Finance Webservice API.
-* Version: 1.0
+* Version: 2.0
 * Author: Juliana Gonçalves
 * License: MIT
 */
@@ -15,14 +15,16 @@ define("CEPATH", WP_PLUGIN_DIR."/".dirname(plugin_basename(__FILE__)));
 function ceCalculator($atts, $content = null) {
     ob_start();
     do_action("include_assets_ce");
-    $template = require(__DIR__ . "/assets/templates/ce-calculator.phtml");
+    $template = require(__DIR__ . "/ce-calculator.phtml");
     return str_replace(array("\r","\n","\t"),"",trim(ob_get_clean()));
 }
 add_shortcode("ce-calculator", "ceCalculator");
 
 function ceAssets() {
-    wp_enqueue_style("ce_flags_css", plugins_url("/assets/flags/flags.css", __FILE__ ));
-    wp_enqueue_style("ce_min_css", plugins_url("/assets/css/currencyExchange.min.css", __FILE__ ));
-    wp_enqueue_script("ce_app_js", plugins_url("/assets/js/currencyExchange.min.js", __FILE__ ), array() , false);
+    wp_enqueue_style("ce_min_css", plugins_url("/currency-exchange-js/currencyExchange.min.css", __FILE__ ));
+	if (!wp_script_is("angular.min.js", "registered") && !wp_script_is("angular.min.js", "enqueued")) {
+	    wp_enqueue_script("angular", "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.1/angular.min.js", array() , false);
+	}
+    wp_enqueue_script("ce_app_js", plugins_url("/currency-exchange-js/currencyExchange.min.js", __FILE__ ), array() , false);
 }
 add_action("include_assets_ce", "ceAssets");
